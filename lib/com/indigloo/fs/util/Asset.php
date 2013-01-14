f654175e1066f790c39fdf8b4d7350ec0d5d0ead
rjha <jha.rajeev@gmail.com>
2013-01-14 19:26:34 +0530

    favsales site : draft3: added home page image and other Home changes

--- lib/com/indigloo/fs/util/Asset.php
+++ lib/com/indigloo/fs/util/Asset.php
@@ -22,11 +22,46 @@ namespace com\indigloo\fs\util{
             return $fname ;
         }
 
+         private static function getTimeStampName($path) {
+            
+            $fname = $path ;
+            $parts = \pathinfo($path);
+            //supplied path is relative to APP_WEB_DIR
+            $fullpath = APP_WEB_DIR.$path ;
+            $ts = 1 ;
+            
+            if(\file_exists($fullpath)) {
+                $ts = \filemtime($fullpath);
+                $ts = "t".$ts ;
+                \settype($ts,"string");
+                // 10 digit unix timestamp cover from
+                // 09 sept. 2001 - 20 Nov. 2286
+                // $perl -MPOSIX -le 'print ctime(1000000000)' 
+
+                $length = \strlen($ts);
+                if($length != 11 ) {
+                    $message = "Asset versioning timestamp is out of range" ;
+                    throw new \Exception($message);
+                }
+
+                // dir/file.ts.min.extension
+                // fs-bundle.css becomes fs-bundle.min.<ts>.css
+
+                $template = "%s/%s.min.%s.%s" ;
+                $fname = sprintf($template,$parts["dirname"],$parts["filename"],$ts,$parts["extension"]);
+                
+            }
+
+            return $fname ;
+        }
+
         static function version($path) {
             $link = '' ;
-            $fname = self::getMinified($path) ;
+            $fname = self::getTimeStampName($path) ;
             
             $parts = \pathinfo($path);
+
+
             if(\strcasecmp($parts["extension"],"css") == 0 ) {
                 $tmpl = '<link rel="stylesheet" type="text/css" href="{fname}" >' ;
                 $link = \str_replace("{fname}",$fname,$tmpl);
--- web/index.php
+++ web/index.php
@@ -22,56 +22,87 @@
         <div class="container mh600">
              <?php include(APP_WEB_DIR . '/app/inc/top/site.inc'); ?>
             <div class="row">
-                <div class="span4 offset1">
-                     <!-- image -->
-                     BIG_IMAGE_COLUMN_1
-                </div> <!-- column:1 -->
-
-                <div class="span4">
-                    <div style="pt100">
-                        <h3> what is favesales</h3>
-                        <p class="comment-text">
-                            Favsales helps businesses to sell directly to fans through Facebook comment. 
-                            It is simple to use and registration takes 30 secs!!!
-
-                        </p>
-                        <h3> How it benefits your business</h3>
-                        <ul class="comment-text">
-                            <li> Facebook comments increase sales as impulse buying increases. This is better than posting a
-website or other link on Facebook.</li>
-                            <li> You can seamlessly sell to their existing fans and friends who are more likely to browse and
-purchase from their pages </li>
-                            <li> You can list unsold inventory even though you sell via other channels </li>
-                            <li> You can get an advantage of selling to better audience on your Facebook fan pages than other
-channels.</li>
-                            <li> You can easily run an auction for limited items </li>
-                            <li> For buyers purchase friction is reduced as they do not have to figure out where and how to
-purchase the products </li>
-                            <li>Reduced cost for merchants compared to other ecommerce stores </li>
-                            <li> Better than Facebook stores as this is integrated seamlessly with the Fan page and the buyer
-notices no difference. You can keep on posting on your fan page where you do not want to sell.
-                            </li>
-                            <li>Preorders can easily be taken and inventory managed better. </li>
-                            <li> No lost sales as people can order and you get know the demand </li>
-                            <li> Lead generation - You get to know people to whom you cannot supply and you can offer them
-alternative products        </li>
-
-                        </ul>
-
-
-                    </div>
-                   
-                </div> <!-- column:2 -->
-
+                <div class="span8 offset1">
+                 <div class="box-shadow p20 mt20">
+                    <!-- image -->
+                    <img src="/site/page/sales-2.jpg" class="aligncenter" alt="main-image" />
+                </div>
             </div> <!-- row:1 -->
+            <div class="row">
+
+                <div class="span8 offset1">
+                   
+                    <h3> what is Favsales</h3>
+                    <p class="lead">
+                        Favsales helps businesses to sell directly to fans through Facebook comment. 
+                        It is simple to use and no setup is required.
+
+                    </p>
+                </div>
+            </div> <!-- row:2 -->
             <div class="row">
                 <div class="span8 offset1">
                     <h3> How it works </h3>
-                    <div class="section">
-                        HOW_IT_WORKS_IMAGE_HORIZONTAL
+                    <div class="wrapper">
+                        <div class="stack">
+                            <span class="badge badge-warning">1</span>
+                            Upload items on facebook
+                            <br>
+                            <img src="/site/page/explain-1.png" />
+                        </div>
+                         <div class="stack">
+                            <span class="badge badge-warning">2</span>
+                            Your fans purchase by commenting on posts.
+                            <br>
+                            <img src="/site/page/explain-2.png" />
+                        </div>
+                         <div class="stack">
+                            <span class="badge badge-warning">3</span>
+                            Favsales does the processing and your receive 
+                            payment in your bank account!
+                            <br>
+                            <img src="/site/page/explain-3.png" />
+                        </div>
+                        <div class="clear">&nbsp; </div>
+
+                    </div>
+                    
+                </div>
+            </div> <!-- row:3 -->
+
+
+            <div class="row">
+                <div class="span8 offset1">
+                    <h3> How it benefits your business</h3>
+                    <div class="hr-red">&nbsp;</div>
+
+                    <p class="lead">
+                     Facebook comments increase sales as impulse buying increases. 
+                     You can seamlessly sell to existing fans and friends who are more 
+                     likely to browse and purchase from your Facebook pages 
+                    </p>
+                    <p class="lead">
+                        Integrated seamlessly with your Facebook Fan page. 
+                        You can keep on posting on your fan page even 
+                        when you do not want to sell and use it parallely to market 
+                        and engage fans. Your fans will never see any difference.
+                    </p>
+                    <p class="lead">
+                        Many other sales activities like running an auction, 
+                        running a end of season sale, preorder for new products, 
+                        flash sales can easily be done.
+                    </p>
+                    <div class="widget">
+                        <p class="lead">
+                            To buy or to know more send an email to 
+                            <a href="mailto:support@favsales.com">support@favsales.com</a>
+                        </p>
+
                     </div>
                 </div>
-            </div> <!-- row:2 -->
+            </div> <!-- row:4 -->
+
+
         </div>   <!-- container -->
 
         <?php include(APP_WEB_DIR . '/app/inc/footer.inc'); ?>
